825c7ce6fdf949946dcf053865d74f9e707f813c
Kala Wetzler <kala.wetzler@gmail.com>
2017-07-19 10:59:59 -0700

    First test for empty collection fails

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-
+  let(:quizikal) { Question.new }
   describe '.recent' do
 
-    it 'displays empty collections if no questions have been created'
+    it 'displays empty collections if no questions have been created' do
+      expect(Question.recent).to be_empty
+    end
+
     it 'displays collection of recent questions that have been created'
 
   end
