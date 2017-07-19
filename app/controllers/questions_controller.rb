7229e55f6606a968d287a52ee386664af899ecc2
Kala Wetzler <kala.wetzler@gmail.com>
2017-07-19 16:25:07 -0700

    Include all verbs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,10 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
   def create
     @question = Question.new(question_params)
     if @question.save
@@ -19,6 +23,22 @@ class QuestionsController < ApplicationController
     else
       render :new, status: 422
     end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+
+    redirect_to questions_path
   end
 
   private
